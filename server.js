99e3e8b68bd2d0807d3096111af8c3d052240069
shogun87 <45832125+shogun87@users.noreply.github.com>
2022-03-11 12:03:49 -0700

    final commit for hw3

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ router.post('/signup', function(req, res) {
         user.save(function(err){
             if (err) {
                 if (err.code === 11000)
-                    return res.json({success: false, message: 'A user with that username already exists'});
+                    return res.status(400).json({success: false, message: 'A user with that username already exists'});
                 else
                     return res.json(err);
             }
